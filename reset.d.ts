5922839cb581c5b2fafb0ed4732901a8c9b27d67
subrat gupta <guptasubratkumar@gmail.com>
2023-06-05 00:54:55 +0530

    plane , ray casting , csg split

---
+++ reset.d.ts
@@ -0,0 +1,16 @@
+// Do not add any other lines of code to this file!
+import "@total-typescript/ts-reset";
+// new Vector3(
+//     translatePointer(point1).x,
+//     translatePointer(point1).y,
+//     camera.position.clone().z
+//   ),
+//   new Vector3(
+//     translatePointer(point2).x,
+//     translatePointer(point2).y,
+//     camera.position.clone().z
+//   ),
+// const centroid = ((p1:Vector3 ,p2:Vector3)=>{p1.clone().add(p2).multiplyScalar(0.5);})(
+//     rayDir1,
+//     rayDir2,
+//   )
